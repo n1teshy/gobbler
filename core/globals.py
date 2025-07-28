0d325927062773e14e5e08534a5b70ca8716accf
Nitesh Yadav <nitesh.y@zifornd.com>
2025-07-28 14:07:26 +0530

    set default CLIP threshold to 0.65

--- core/globals.py
+++ core/globals.py
@@ -3,4 +3,4 @@ import os
 core_dir = os.path.dirname(__file__)
 instructions_dir = os.path.join(core_dir, "instructions")
 
-clip_prob_thresh = os.getenv("CLIP_THRESHOLD", 0.7)
+clip_prob_thresh = os.getenv("CLIP_THRESHOLD", 0.65)
