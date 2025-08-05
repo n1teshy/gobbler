d39dc20b0eb8262e891160e712d7f10a8b14bbbe
Nitesh Yadav <nitesh.y@zifornd.com>
2025-08-05 12:21:21 +0530

    remove version requirement from 'scikit-image' dep

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "python-dotenv",
         "transformers==4.40.0",
         "opencv-python==4.10.0.84",
-        "scikit-image==0.20.0",
+        "scikit-image",
         "numpy==1.26.0",
         "pydantic",
         "openai==1.88.0",
